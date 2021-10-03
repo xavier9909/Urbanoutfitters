e993cdcbffec7e69ad892c9ae10d9254f9980545
xavier9909 <57476138+xavier9909@users.noreply.github.com>
2021-10-03 20:53:21 +0530

    Add files via upload

---
+++ sign.js
@@ -0,0 +1,8 @@
+
+         function togglePopup() {
+            document.getElementById("popup-1").classList.toggle("active");
+       } 
+       function togglePopup1() {
+         document.getElementById("popup-2").classList.toggle("active");
+    } 
+  
